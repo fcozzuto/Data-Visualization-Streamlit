975ce5caf501c8a05d75470adb768b03fea21787
fcozzuto <cozzuto.fabio@gmail.com>
2025-03-10 11:10:31 -0400

    Initial app development

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import kagglehub
+import os
+import pandas as pd
+import altair as alt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Streamlit Python Demo Dashboard")
+st.write("This is a simple Dashboard created in Python, using the streamlit module and its functionalities.")
+
+path = kagglehub.dataset_download("ahmedmohamed2003/quality-of-life-for-each-country")
+print(os.listdir(path))
+
+df = pd.read_csv(path + "/Quality_of_Life.csv")
+
+# Setting up the parameters to display the chart 
+chart = alt.Chart(df).mark_line().encode(
+    x=alt.X("country", title="Country"),
+    y=alt.Y("Safety Value", title="Safety Value")
+).properties(
+    title="Safety Value by Country"
 )
+
+# Displaying the chart in Streamlit
+st.altair_chart(chart, use_container_width=True)
